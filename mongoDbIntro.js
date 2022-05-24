c894509f3bd30398399b4908426fd3d57f89f573
Hansel Reynoso <omarreynoso09@gmail.com>
2022-05-24 15:05:50 -0400

    initial commit first part

---
+++ mongoDbIntro.js
@@ -0,0 +1,22 @@
+// db.collection.count()
+// let blogPost = {
+//     createAt:new Date().toString(),
+//     title:"first blog post",
+//     text:"this is a blog post for training purpose",
+//     author:"hansel reynoso",
+//     id:String(db.posts.count()+1 )
+// }
+
+// db.posts.insertOne(blogPost);
+
+// db.posts.find(
+//     {
+//         author: "hansel reynoso"
+//     })
+
+// db.posts.updateOne({title: "first blog post"},
+// {$set:{ title:"Updated First Blog Post"}})
+
+// db.posts.deleteOne(
+//       { id:String(db.posts.count())}
+//       )
